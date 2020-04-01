bdffff117913d862788fbdc1187d91e94b0aeb5c
zoton2 <zoton2@gmail.com>
2020-04-01 13:53:31 +0100

    Code copied over from esa-layouts

--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable global-require */
+
 import { NodeCG } from 'nodecg/types/server';
 import { set } from './util/nodecg';
 
 export = (nodecg: NodeCG): void => {
   set(nodecg);
-  // do logic
+  require('./fcb');
 };
